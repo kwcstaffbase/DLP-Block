9524588f3287aeb533396a1e5c18b07e0b96129d
kwcstaffbase <kelvin.cheng@staffbase.com>
2026-05-05 12:37:57 -0600

    Implement redirect for download dialog
    Add a mutation observer to redirect on download dialog

--- dlpblock.js
+++ dlpblock.js
@@ -1,5 +1,16 @@
 (function () {
-  if (window.location.pathname === '/download') {
-    window.location.href = 'companyportal://apps';
+  var redirected = false;
+
+  function checkAndRedirect() {
+    if (!redirected && document.querySelector('[aria-label="Download"][role="dialog"]')) {
+      redirected = true;
+      observer.disconnect();
+      window.location.href = 'companyportal://apps';
+    }
   }
+
+  var observer = new MutationObserver(checkAndRedirect);
+  observer.observe(document.body, { childList: true, subtree: true });
+
+  checkAndRedirect();
 })();
